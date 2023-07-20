89898d4bbda6fbe8258052dbbad7582c0bdd7251
Maria Spiridonova <traffic2007@mail.ru>
2023-07-20 23:04:37 +0300

    sprint13-1

--- app.js
+++ app.js
@@ -10,18 +10,16 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
 const app = express();
-
-app.use(bodyParser.json());
-app.use(userRouter);
-app.use(cardRouter);
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use((req, res, next) => {
   req.user = {
     _id: '64b92fa557d20e3dd083c24d',
   };
   next();
 });
+
+app.use(bodyParser.json());
+app.use(userRouter);
+app.use(cardRouter);
 
 app.use('*', (req, res) => {
   res.status(400).send({ message: 'Страница не найдена' });
