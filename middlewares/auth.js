c34966c91210e48167d9e32e80137e09d697b4a9
Maria Spiridonova <traffic2007@mail.ru>
2023-08-01 00:32:58 +0300

    14sprint1

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,21 +1,19 @@
 const jwt = require('jsonwebtoken');
 
+const Unauthorized = require('../errors/Unauthorized');
+
 function auth(req, res, next) {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer: ')) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    next(new Unauthorized('Необходима авторизация'));
   }
   const token = authorization.replace('Bearer: ', '');
   let payload;
   try {
     payload = jwt.verify(token, 'super-strong-secret');
   } catch (err) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    next(new Unauthorized('Необходима авторизация'));
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
