5c6857545bf9a2f569fe22aec67f86be14163688
Maria Spiridonova <traffic2007@mail.ru>
2023-08-01 16:46:46 +0300

    14

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -10,7 +10,7 @@ const auth = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(token, 'super-strong-secret');
   } catch (err) {
     return next(new Unauthorized('Необходима авторизация'));
   }
